4ad91fa5019651ab15d91fd2e39166a9800cc0ea
charleszq@gmail.com <charleszq@gmail.com@afc585b9-fde5-bfaf-e0be-94fb11a9bf30>
2011-06-15 09:43:08 +0000

    try to fix the issue 5, the PhotoListFragment, still need to handle the photo detail page

--- trunk/src/com/charles/dataprovider/IPhotoListDataProvider.java
+++ trunk/src/com/charles/dataprovider/IPhotoListDataProvider.java
@@ -1,18 +1,24 @@
+
 package com.charles.dataprovider;
 
 import com.aetrion.flickr.photos.PhotoList;
+import java.io.Serializable;
 
 /**
  * Represents the data provider to provide a list of photos.
+ * <p>
+ * Make it extend from <code>Serializable</code> is to save this when fragment
+ * is to be destroyed.
  * 
  * @author charles
  */
-public interface IPhotoListDataProvider {
+public interface IPhotoListDataProvider extends Serializable {
 
-	/**
-	 * Returns a list of photos.
-	 * @return
-	 * @throws Exception
-	 */
-	PhotoList getPhotoList() throws Exception;
+    /**
+     * Returns a list of photos.
+     * 
+     * @return
+     * @throws Exception
+     */
+    PhotoList getPhotoList() throws Exception;
 }
