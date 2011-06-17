e98f7c1383abe70797a67eaa051fc745b67891e7
yuyang226 <yuyang226@gmail.com>
2011-06-17 13:30:06 +0000

    properly show the HTML tags for comments

--- src/com/charles/ui/ViewImageDetailFragment.java
+++ src/com/charles/ui/ViewImageDetailFragment.java
@@ -14,6 +14,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -296,7 +297,7 @@ public class ViewImageDetailFragment extends Fragment implements
 
 			UserComment userComment = (UserComment) getItem(position);
 			author.setText(userComment.getUserName());
-			comment.setText(userComment.getCommentText());
+			comment.setText(Html.fromHtml(userComment.getCommentText()));
 			commentDate.setText(userComment.getCommentDateString());
 
 			Drawable drawable = buddyIcon.getDrawable();
