6b449d1d201fabdc63d1dde262d7c43ad3da442d
charleszq@gmail.com <charleszq@gmail.com@afc585b9-fde5-bfaf-e0be-94fb11a9bf30>
2011-07-15 08:26:28 +0000

    bug fix, make the task cancellable.

--- trunk/src/com/charles/task/WriteCommentTask.java
+++ trunk/src/com/charles/task/WriteCommentTask.java
@@ -1,70 +1,55 @@
 /**
  * 
  */
-package com.charles.task;
 
-import android.app.DialogFragment;
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
-import android.widget.Toast;
+package com.charles.task;
 
 import com.aetrion.flickr.Flickr;
 import com.aetrion.flickr.photos.comments.CommentsInterface;
 import com.charles.utils.FlickrHelper;
 
+import android.app.DialogFragment;
+import android.widget.Toast;
+
 /**
  * @author charles
- *
  */
-public class WriteCommentTask extends AsyncTask<String, Integer, Boolean> {
-	
-	private String mToken; 
-	private DialogFragment mDialog;
-	private ProgressDialog mProgressDialog;
-	
-	public WriteCommentTask(String token, DialogFragment dialog ) {
-		this.mToken = token;
-		mDialog = dialog;
-	}
-	
-	
+public class WriteCommentTask extends ProgressDialogAsyncTask<String, Integer, Boolean> {
 
-	@Override
-	protected void onPreExecute() {
-		super.onPreExecute();
-		mProgressDialog = ProgressDialog.show(mDialog.getActivity(), "", "Adding comment...");
-	}
+    private static final String MSG = "Adding comment...";
 
+    private String mToken;
+    private DialogFragment mDialog;
 
+    public WriteCommentTask(String token, DialogFragment dialog) {
+        super(dialog.getActivity(), MSG);
+        this.mToken = token;
+        mDialog = dialog;
+    }
 
-	@Override
-	protected Boolean doInBackground(String... params) {
-		String photoId = params[0];
-		String comment = params[1];
-		
-		Flickr f = FlickrHelper.getInstance().getFlickrAuthed(mToken);
-		CommentsInterface ci = f.getCommentsInterface();
-		try {
-			ci.addComment(photoId, comment);
-		} catch (Exception e) {
-			return false;
-		}
-		return true;
-	}
+    @Override
+    protected Boolean doInBackground(String... params) {
+        String photoId = params[0];
+        String comment = params[1];
 
-	@Override
-	protected void onPostExecute(Boolean result) {
-		mDialog.dismiss();
-		if( mProgressDialog != null && mProgressDialog.isShowing() ) {
-			mProgressDialog.dismiss();
-		}
-		String msg = "Comment added.";
-		if( !result ) {
-			msg = "Error when adding comment";
-		}
-		Toast.makeText(mDialog.getActivity(), msg, Toast.LENGTH_SHORT).show();
-	}
-	
-	
+        Flickr f = FlickrHelper.getInstance().getFlickrAuthed(mToken);
+        CommentsInterface ci = f.getCommentsInterface();
+        try {
+            ci.addComment(photoId, comment);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
+        String msg = "Comment added.";
+        if (!result) {
+            msg = "Error when adding comment";
+        }
+        Toast.makeText(mDialog.getActivity(), msg, Toast.LENGTH_SHORT).show();
+    }
 
 }
