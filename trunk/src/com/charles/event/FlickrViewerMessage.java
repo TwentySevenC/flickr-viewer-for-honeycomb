3424ca9776d0e59e3f4ae12b73f3bd0b115d9502
charleszq@gmail.com <charleszq@gmail.com@afc585b9-fde5-bfaf-e0be-94fb11a9bf30>
2011-07-26 03:48:36 +0000

    after remove favorite, refresh the photo list.

---
+++ trunk/src/com/charles/event/FlickrViewerMessage.java
@@ -0,0 +1,36 @@
+/*
+ * Created on Jul 26, 2011
+ *
+ * Copyright (c) Sybase, Inc. 2011   
+ * All rights reserved.                                    
+ */
+
+package com.charles.event;
+
+/**
+ * represents the message data structure.
+ * 
+ * @author charles
+ *
+ */
+public final class FlickrViewerMessage {
+    
+    public static final String FAV_PHOTO_REMOVED = "fav.photo.removed"; //$NON-NLS-1$
+    
+    private String mMessageId;
+    private Object mMessageData;
+
+    public FlickrViewerMessage(String id, Object data) {
+        this.mMessageId = id;
+        this.mMessageData = data;
+    }
+
+    public String getMessageId() {
+        return mMessageId;
+    }
+
+    public Object getMessageData() {
+        return mMessageData;
+    }
+    
+}
