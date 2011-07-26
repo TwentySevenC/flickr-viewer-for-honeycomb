e9d0a582fb7e46a19a6dcbb2aa23ca11de9ca882
charleszq <charleszq@gmail.com>
2011-07-26 03:58:58 +0000

    bug fix, don't searialize the photo list for the photo list providers.

--- src/com/charles/dataprovider/PaginationPhotoListDataProvider.java
+++ src/com/charles/dataprovider/PaginationPhotoListDataProvider.java
@@ -1,6 +1,7 @@
 /**
  * 
  */
+
 package com.charles.dataprovider;
 
 import com.aetrion.flickr.photos.PhotoList;
@@ -12,35 +13,38 @@ import android.content.Context;
  * Represents the photo list data provider which has the pagination feature.
  * 
  * @author charles
- * 
  */
 public abstract class PaginationPhotoListDataProvider implements
-		IPhotoListDataProvider {
+        IPhotoListDataProvider {
 
-	/**
-	 * auto generated sid.
-	 */
-	private static final long serialVersionUID = 4702763028164978288L;
-	
-	protected int mPageSize = Constants.DEF_GRID_PAGE_SIZE;
-	protected int mPageNumber = 1;
-	protected PhotoList mPhotoList = null;
+    /**
+     * auto generated sid.
+     */
+    private static final long serialVersionUID = 4702763028164978288L;
 
-	public void setPageSize(int mPageSize) {
-		this.mPageSize = mPageSize;
-	}
+    protected int mPageSize = Constants.DEF_GRID_PAGE_SIZE;
+    protected int mPageNumber = 1;
+    
+    /**
+     * Don't serialize this. 
+     */
+    transient protected PhotoList mPhotoList = null;
 
-	public void setPageNumber(int mPageNumber) {
-		this.mPageNumber = mPageNumber;
-	}
-	
-	@Override
+    public void setPageSize(int mPageSize) {
+        this.mPageSize = mPageSize;
+    }
+
+    public void setPageNumber(int mPageNumber) {
+        this.mPageNumber = mPageNumber;
+    }
+
+    @Override
     public void invalidatePhotoList() {
-	    this.mPhotoList = null;
+        this.mPhotoList = null;
     }
 
     /**
-	 * @return
-	 */
-	public abstract String getDescription(Context context);
+     * @return
+     */
+    public abstract String getDescription(Context context);
 }
