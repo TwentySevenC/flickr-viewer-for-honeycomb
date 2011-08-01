450e304caaf2c52711a41a6051a3c29164f4ae4d
charleszq@gmail.com <charleszq@gmail.com@afc585b9-fde5-bfaf-e0be-94fb11a9bf30>
2011-08-01 04:37:59 +0000

    make the links in photo description clickable, that is, the <a href="xxx">Link Text</a>.

--- trunk/src/com/gmail/charleszq/utils/StringUtils.java
+++ trunk/src/com/gmail/charleszq/utils/StringUtils.java
@@ -21,41 +21,74 @@ import java.util.regex.Pattern;
  * @author charles
  */
 public final class StringUtils {
-    
-    private static final String TAG = StringUtils.class.getName();
 
-    /**
-     * Example: [http://www.flickr.com/photos/example/2910192942/]
-     */
-    private static final String FILICK_URL_EXPRESSION = "(\\[http){1}+(s)?+(://){1}+.*\\]{1}+"; //$NON-NLS-1$
+	private static final String TAG = StringUtils.class.getName();
 
-    public static void formatHtmlString(String string, TextView textView) {
-        
-        Log.d(TAG, string);
-        
-        textView.setText(Html.fromHtml(string));
-        Linkify.addLinks(textView, Pattern.compile(FILICK_URL_EXPRESSION),
-                "http://", new MatchFilter() { //$NON-NLS-1$
+	/**
+	 * Example: [http://www.flickr.com/photos/example/2910192942/]
+	 */
+	private static final String FILICK_URL_EXPRESSION = "(\\[http){1}+(s)?+(://){1}+.*\\]{1}+"; //$NON-NLS-1$
 
-                    @Override
-                    public boolean acceptMatch(CharSequence s, int start,
-                            int end) {
-                        return true;
-                    }
+	/**
+	 * Example: <a href="....">Link Text</a>
+	 */
+	private static final String A_HREF_STRING_EXP = "<a\\b[^>]*href=\"[^>]*>(.*?)</a>"; //$NON-NLS-1$
 
-                }, new TransformFilter() {
+	/**
+	 * The pattern to retrieve the real link part.
+	 */
+	private static Pattern hrefPattern = Pattern.compile("href=\"[^>]*\">"); //$NON-NLS-1$
 
-                    @Override
-                    public String transformUrl(Matcher matcher, String data) {
-                        if (data.length() > 2) {
-                            return data.substring(1, data.length() - 1);
-                        }
-                        return data;
-                    }
+	public static void formatHtmlString(String string, TextView textView) {
 
-                });
-    }
+		Log.d(TAG, string);
 
-    private StringUtils() {
-    }
+		textView.setText(Html.fromHtml(string));
+		// textView.setText(string);
+		Linkify.addLinks(textView, Pattern.compile(FILICK_URL_EXPRESSION),
+				"http://", new MatchFilter() { //$NON-NLS-1$
+
+					@Override
+					public boolean acceptMatch(CharSequence s, int start,
+							int end) {
+						return true;
+					}
+
+				}, new TransformFilter() {
+
+					@Override
+					public String transformUrl(Matcher matcher, String data) {
+						if (data.length() > 2) {
+							return data.substring(1, data.length() - 1);
+						}
+						return data;
+					}
+
+				});
+
+		Linkify.addLinks(textView, Pattern.compile(A_HREF_STRING_EXP),
+				"http://", new MatchFilter() { //$NON-NLS-1$
+
+					@Override
+					public boolean acceptMatch(CharSequence s, int start,
+							int end) {
+						return true;
+					}
+				}, new TransformFilter() {
+
+					@Override
+					public String transformUrl(Matcher match, String url) {
+						Matcher matcher = hrefPattern.matcher(match.group());
+						matcher.find();
+						String s = matcher.group().replaceFirst("href=\"", "")  //$NON-NLS-1$//$NON-NLS-2$
+								.replaceFirst("\">", "");  //$NON-NLS-1$//$NON-NLS-2$
+						Log.d(TAG, "link = " + s); //$NON-NLS-1$
+						return s;
+					}
+				});
+
+	}
+
+	private StringUtils() {
+	}
 }
