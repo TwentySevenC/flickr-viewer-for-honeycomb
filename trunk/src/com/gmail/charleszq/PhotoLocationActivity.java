027f83f3178180fd94215b19d80dd756074d62e0
charleszq@gmail.com <charleszq@gmail.com@afc585b9-fde5-bfaf-e0be-94fb11a9bf30>
2011-08-23 15:03:33 +0000

    bug fix: map view zoom make the pushpin show at wrong position

--- trunk/src/com/gmail/charleszq/PhotoLocationActivity.java
+++ trunk/src/com/gmail/charleszq/PhotoLocationActivity.java
@@ -9,6 +9,7 @@ package com.gmail.charleszq;
 
 import java.util.List;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -17,6 +18,7 @@ import android.graphics.Canvas;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.gmail.charleszq.utils.ImageUtils;
 import com.google.android.maps.GeoPoint;
@@ -34,9 +36,31 @@ import com.google.android.maps.Overlay;
 public class PhotoLocationActivity extends MapActivity {
 
 	private static final String TAG = PhotoLocationActivity.class.getName();
+	private static final int INVALID_LAT_LNG_VAL = (int) (360 * 1E6);
 
 	public static final String LAT_VAL = "lat"; //$NON-NLS-1$
 	public static final String LONG_VAL = "long"; //$NON-NLS-1$
+
+	/**
+	 * The zoom level of the map view.
+	 */
+	private int mZoomLevel = 15;
+
+	/**
+	 * The map view.
+	 */
+	private MapView mMapView;
+
+	/**
+	 * The photo location information.
+	 */
+	private int mLatitude = INVALID_LAT_LNG_VAL,
+			mLongtitude = INVALID_LAT_LNG_VAL;
+
+	/**
+	 * the cache geo point instance.
+	 */
+	private GeoPoint mPhotoGeoPoint = null;
 
 	/*
 	 * (non-Javadoc)
@@ -46,6 +70,23 @@ public class PhotoLocationActivity extends MapActivity {
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			finish();
+			return true;
+		} else {
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 	/*
@@ -58,24 +99,66 @@ public class PhotoLocationActivity extends MapActivity {
 		super.onCreate(icicle);
 		setContentView(R.layout.photo_map);
 		Intent intent = getIntent();
-		int lat = intent.getExtras().getInt(LAT_VAL);
-		int lng = intent.getExtras().getInt(LONG_VAL);
 
-		Log.d(TAG, "location: " + lat + ", " + lng); //$NON-NLS-1$//$NON-NLS-2$
-		MapView map = (MapView) findViewById(R.id.mapView);
+		if (icicle != null) {
+			mLatitude = icicle.getInt(LAT_VAL);
+			mLongtitude = icicle.getInt(LONG_VAL);
+		} else {
+			mLatitude = intent.getExtras().getInt(LAT_VAL);
+			mLongtitude = intent.getExtras().getInt(LONG_VAL);
+		}
 
-		MapController mc = map.getController();
-		GeoPoint p = new GeoPoint(lat, lng);
+		mMapView = (MapView) findViewById(R.id.mapView);
 
-		mc.setCenter(p);
-		mc.setZoom(15);
+		MapController mc = mMapView.getController();
+		mPhotoGeoPoint = new GeoPoint(mLatitude, mLongtitude);
 
-		MapOverlay mapOverlay = new MapOverlay(this, p);
-		List<Overlay> listOfOverlays = map.getOverlays();
+		mc.animateTo(mPhotoGeoPoint);
+		mc.setZoom(mZoomLevel);
+
+		redrawPushpin();
+	}
+
+	@Override
+	public void onUserInteraction() {
+		super.onUserInteraction();
+		if (mMapView != null) {
+			int zoomLevel = mMapView.getZoomLevel();
+			if (zoomLevel != mZoomLevel) {
+				redrawPushpin();
+				mZoomLevel = zoomLevel;
+				Log.d(TAG, "Map view zoom level changed."); //$NON-NLS-1$
+			}
+		}
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putInt(LAT_VAL, mLatitude);
+		outState.putInt(LONG_VAL, mLongtitude);
+	}
+
+	/**
+	 * Redraws the push pin on the map view.
+	 */
+	private void redrawPushpin() {
+		if (mPhotoGeoPoint == null) {
+			if (mLatitude != INVALID_LAT_LNG_VAL
+					&& mLongtitude != INVALID_LAT_LNG_VAL)
+				mPhotoGeoPoint = new GeoPoint(mLatitude, mLongtitude);
+		}
+
+		if (mPhotoGeoPoint == null) {
+			return;
+		}
+
+		MapOverlay mapOverlay = new MapOverlay(this, mPhotoGeoPoint);
+		List<Overlay> listOfOverlays = mMapView.getOverlays();
 		listOfOverlays.clear();
 		listOfOverlays.add(mapOverlay);
 
-		map.invalidate();
+		mMapView.invalidate();
 	}
 
 	private static class MapOverlay extends com.google.android.maps.Overlay {
@@ -95,13 +178,20 @@ public class PhotoLocationActivity extends MapActivity {
 
 			// ---translate the GeoPoint to screen pixels---
 			Point screenPts = new Point();
-			mapView.getProjection().toPixels(mPosition, screenPts);
+
+			if (mPosition == null) {
+				screenPts.x = screenPts.y = 0;
+			} else {
+				mapView.getProjection().toPixels(mPosition, screenPts);
+			}
 
 			// ---add the marker---
 			Bitmap bmp = BitmapFactory.decodeResource(mContext.getResources(),
 					R.drawable.pushpin);
 			Bitmap resizedBitmap = ImageUtils.resize(bmp, 0.3f);
-			canvas.drawBitmap(resizedBitmap, screenPts.x, screenPts.y - 150,
+
+			// 90 = image_org_height * 0.3
+			canvas.drawBitmap(resizedBitmap, screenPts.x, screenPts.y - 90,
 					null);
 			return true;
 		}
