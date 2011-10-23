b8a0a0f7e0b53da5b6899babdebd2a8d42a517bc
yuyang226@gmail.com <yuyang226@gmail.com@afc585b9-fde5-bfaf-e0be-94fb11a9bf30>
2011-10-23 05:28:06 +0000

    v1.2.3

---
+++ tags/1.2.3/src/com/gmail/charleszq/utils/ImageCache.java
@@ -0,0 +1,55 @@
+package com.gmail.charleszq.utils;
+
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+
+import android.graphics.Bitmap;
+
+public final class ImageCache {
+
+	private static Map<String, Bitmap> cache = new ConcurrentHashMap<String, Bitmap>();
+	private static Queue<String> queue = new LinkedList<String>();
+
+	public static int CACHE_SIZE = Constants.DEF_CACHE_SIZE;
+
+	public static void dispose() {
+		for (Bitmap bm : cache.values()) {
+			if (bm != null) {
+				bm.recycle();
+			}
+		}
+		cache.clear();
+	}
+
+	public static void saveToCache(String url, Bitmap bitmap) {
+
+		if (url == null || bitmap == null || bitmap.isRecycled()) {
+			return;
+		}
+
+		if (cache.size() >= CACHE_SIZE) {
+			String firstKey = queue.poll();
+			Bitmap toBeRemoved = cache.get(firstKey);
+			cache.remove(firstKey);
+			if (toBeRemoved != null) {
+				// toBeRemoved.recycle();
+				toBeRemoved = null;
+			}
+		}
+
+		cache.put(url, bitmap);
+		queue.add(url);
+	}
+
+	public static Bitmap getFromCache(String url) {
+		Bitmap bitmap = cache.get(url);
+		if (bitmap == null || bitmap.isRecycled()) {
+			cache.remove(url);
+			bitmap = null;
+		}
+
+		return bitmap;
+	}
+}
